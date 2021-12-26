366c16f51bbecd2063460ab82b26c2827d8ebe04
cgcostume <cgdaniel@outlook.com>
2021-12-26 18:50:25 +0100

    expose jsonschema, invalidate, and msagent

--- source/index.ts
+++ source/index.ts
@@ -5,8 +5,14 @@
 export * from './about';
 
 export * from './auxiliaries';
+
+export { default as jsonschema } from 'jsonschema';
+
 export * from './changelookup';
 export * as properties from './properties';
 export * from './fetch';
+
+export * as msagent from './msagent';
+export * from './invalidate';
 
 /* spellchecker: enable */
